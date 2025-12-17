351d19a5208a6ebeb6539a49c4608b31017de030
Thomas Davis <thomas.medavis@gmail.com>
2025-12-17 01:25:15 -0500

    Ticket-1: Initial Spring Boot project setup

---
+++ src/main/java/th/tickethub/TicketHubApplication.java
@@ -0,0 +1,13 @@
+package th.tickethub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketHubApplication.class, args);
+    }
+
+}
