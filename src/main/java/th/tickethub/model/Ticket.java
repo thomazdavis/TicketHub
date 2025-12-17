a9f1e99a920f4f9ac90239d0c871fd3da28c4d7e
Thomas Davis <thomas.medavis@gmail.com>
2025-12-17 17:33:58 -0500

    Ticket-4: Added Ticket entity

---
+++ src/main/java/th/tickethub/model/Ticket.java
@@ -0,0 +1,27 @@
+package th.tickethub.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String seatNumber;
+
+    private String ownerName;
+
+    // To prevent 2 people from purchasing the same ticket
+    @Version
+    private Integer version;
+}
